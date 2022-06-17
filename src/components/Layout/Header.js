763a4e33376d7f662e36c16fad7ffa406b9e9ed3
unknown <lukacc008@gmail.com>
2022-06-17 16:20:11 +0200

    Formatted some files.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,15 +6,15 @@ import classes from "./Header.module.css";
 
 const Header = (props) => {
   return (
-  <Fragment>
-    <header className={classes.header}>
-      <h1>ReactMeals</h1>
-      <HeaderCartButton />
-    </header>
-    <div className={classes["main-image"]}>
-      <img src={mealsImage} alt="A table full of delicious food!" />
-    </div>
-  </Fragment>
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
   );
 };
 
