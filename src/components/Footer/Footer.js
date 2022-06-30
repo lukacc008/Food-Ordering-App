fc3a445baec440fc2795895d2a6ca7d34c8f1784
unknown <lukacc008@gmail.com>
2022-06-30 16:23:31 +0200

    Added some social-media icons to footer using install react-icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,11 @@
 import classes from "./Footer.module.css";
+import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs"
 
 const Footer = () => {
   return (
   <div className={classes.footer}>
     <h1>FOOTER</h1>
+        <p>Drustvene mreze: <BsFacebook /> <BsInstagram /> <BsTwitter /></p>
   </div>
   )
 };
