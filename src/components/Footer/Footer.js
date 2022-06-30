b3f0b00074ee2b44e1ab23807625eee4670297d6
unknown <lukacc008@gmail.com>
2022-06-30 19:03:47 +0200

    small icon changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,24 @@
 import classes from "./Footer.module.css";
-import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs"
+import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
   return (
-  <div className={classes.footer}>
-    <h1>FOOTER</h1>
-        <p>Drustvene mreze: <BsFacebook /> <BsInstagram /> <BsTwitter /></p>
-  </div>
-  )
+    <div className={classes.footer}>
+      <h1>FOOTER</h1>
+      <div className={classes.kontakti}>
+        {" "}
+        <a href="https://www.facebook.com/">
+          <BsFacebook color="white" size="2em" />
+        </a>{" "}
+        <a href="https://www.instagram.com/">
+          <BsInstagram color="white" size="2em" />
+        </a> {" "}
+        <a href="https://twitter.com/">
+          <BsTwitter color="white" size="2em"/>
+        </a>
+        </div>
+    </div>
+  );
 };
 
 export default Footer;
