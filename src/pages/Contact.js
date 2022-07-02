43ad64603c81dab3a4940c902ebf6bfd71c08833
unknown <lukacc008@gmail.com>
2022-07-02 19:08:28 +0200

    Working on contacts page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,12 @@
-import React, { Fragment } from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React, { Fragment } from "react";
+import Card from "../components/UI/Card";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import classes from "./Contact.module.css";
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width: "",
+  height: "400px",
+  top: "4.10rem",
 };
 
 const center = {
@@ -13,37 +16,40 @@ const center = {
 
 function MyComponent() {
   const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyC9UQQJY9uwZBh8mBc4YE1mzNV0BU-RGaI"
-  })
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyC9UQQJY9uwZBh8mBc4YE1mzNV0BU-RGaI",
+  });
 
-  const [map, setMap] = React.useState(null)
+  const [map, setMap] = React.useState(null);
 
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
-    setMap(map)
-  }, [])
+    setMap(map);
+  }, []);
 
   const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+    setMap(null);
+  }, []);
 
   return isLoaded ? (
-      <Fragment>
-          <h2>Kontakt</h2>
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Svratite do nas!</h1>
+        <h2>Mobilni: 060 000 111 2</h2>
+        <h3>Adresa: Jurija Gagarina 151, Beograd</h3>
+      </header>
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
         zoom={15}
         onLoad={onLoad}
         onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-      </Fragment>
-  ) : <></>
+      ></GoogleMap>
+    </Fragment>
+  ) : (
+    <></>
+  );
 }
 
-export default React.memo(MyComponent)
+export default React.memo(MyComponent);
