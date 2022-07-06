5a3771c4e031f8a0fafe2afeb425a4894f54a44c
unknown <lukacc008@gmail.com>
2022-07-06 18:29:34 +0200

    Styling changes in about-us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -10,6 +10,19 @@ const AboutUs = () => {
       <header className={classes.header}>
         <GiHamburger className={classes.burger}/><h1>BAD GUYS BURGERS</h1><GiHamburger className={classes.burger}/>
       </header>
+
+      <div className={classes.summary}>
+      <h2>Najbolji Burgeri U Gradu Kucaju Na Vaša Vrata</h2>
+      <p>
+        Izaberite vaš omiljeni burger iz naše ponude 
+        i uzivajte u prelepom ručku ili večeri kod kuće!
+      </p>
+      <p>
+        Svi naši burgeri pripremljeni su sa najkvalitetnijim sastojcima, od strane
+        iskusnih burger majstora sa višegodišnjim iskustvom!
+      </p>
+    </div>
+
     <div>
       <ImageSlider slides={SliderData}/>
     </div>
