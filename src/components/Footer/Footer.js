a69ca1cb872b6d042c322614a45cfb81e95278a8
unknown <lukacc008@gmail.com>
2022-07-24 02:28:25 +0200

    Updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,19 @@
 import classes from "./Footer.module.css";
 import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
+import badGuysLogo from "./../../assets/logo.jpeg"
 
 const Footer = () => {
   return (
     <div className={classes.footer}>
-      <h3>Bad-Guys burgers d.o.o.</h3>
+      <div className={classes["logo-image"]}>
+        <img src={badGuysLogo} />
+        </div>
       <div className={classes.kontakti}>
         {" "}
         <a href="https://www.facebook.com/" target="_blank">
           <BsFacebook color="white" size="1em" />
         </a>{" "}
-        <a href="https://www.instagram.com/" target="_blank">
+        <a href="https://www.instagram.com/badguysnbg/" target="_blank">
           <BsInstagram color="white" size="1em"  />
         </a> {" "}
         <a href="https://twitter.com/" target="_blank">
