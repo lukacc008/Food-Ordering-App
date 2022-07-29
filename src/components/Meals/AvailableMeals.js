00de59f22f6500ae0e346d7f294b2061fa48d058
unknown <lukacc008@gmail.com>
2022-07-29 18:25:26 +0200

    Updateds and responsive design

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,40 +1,8 @@
-import Card from "../UI/Card";
-import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
-
-const LISTA_BURGERA = [
-  {
-    id: "m1",
-    name: "Burger Classic",
-    description: "krastavac, luk, kečap, senf",
-    price: 550,
-  },
-  {
-    id: "m2",
-    name: "Burger Original",
-    description: "iceberg, luk, krastavac, burger sos",
-    price: 600,
-  },
-];
+import SlikaBurgera from "./../../assets/slika.jpeg";
 
 const AvailableMeals = () => {
-  const mealsList = LISTA_BURGERA.map((meal) => (
-    <MealItem
-      id={meal.id}
-      key={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
-
-  return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
-  );
+  return <div className={classes.meals}></div>;
 };
 
 export default AvailableMeals;
--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -6,10 +6,6 @@ const MealsSummary = () => {
     <section className={classes.summary}>
       <h2>Najbolji Burgeri U Gradu Kucaju Na Vaša Vrata</h2>
       <p>
-        Izaberite vaš omiljeni burger iz naše ponude 
-        i uzivajte u prelepom ručku ili večeri kod kuće!
-      </p>
-      <p>
         Svi naši burgeri pripremljeni su sa najkvalitetnijim sastojcima, od strane
         iskusnih burger majstora sa višegodišnjim iskustvom!
       </p>
