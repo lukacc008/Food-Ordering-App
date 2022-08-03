3d508c54f4d239b701e52b46e1f27631acdb6ced
unknown <lukacc008@gmail.com>
2022-08-03 17:08:46 +0200

    Added real images/responsive

--- src/components/UI/ImageSliderData.js
+++ src/components/UI/ImageSliderData.js
@@ -1,25 +1,34 @@
+
 const SliderData = [
   {
     image:
-      "https://imageproxy.wolt.com/venue/62820643bacb69b948f3b063/dc06cab6-d5c3-11ec-8c73-c206c158cc01_menu_3__4_.jpg",
+      "https://scontent.fbeg1-1.fna.fbcdn.net/v/t39.30808-6/296100041_115049674615820_6802665747528421938_n.jpg?stp=dst-jpg_s1080x2048&_nc_cat=101&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeHKcfvVzT6wLTPvrelKsDe-7Sh2mgSyyqPtKHaaBLLKo_L43XTOP7ZDKOiC5ZEjpOA&_nc_ohc=NSqlebX6h4MAX-yS_wz&_nc_ht=scontent.fbeg1-1.fna&oh=00_AT_iX979AalnRa8kRiJwxypp5NCXBv_vWUo9VngFhavfMw&oe=62F03B22",
   },
 {
     image:
-      "https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+      "https://scontent.fbeg1-1.fna.fbcdn.net/v/t39.30808-6/295893777_113724501415004_6992504198134337382_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeGAZtxfdvZz7A13y-fM_jbnH6GvDke3e38foa8OR7d7f4H7cbhWUWQklSgPICNNTU8&_nc_ohc=Y279_gLd0NMAX978cLY&tn=3xstOxlhC1v1kjRY&_nc_ht=scontent.fbeg1-1.fna&oh=00_AT9VDUM28p4EIhKs_5uYXNsaLfIasKeiDwNPtUpPa4-heg&oe=62EEACA5",
 },
 {
     image:
-      "https://images.unsplash.com/photo-1561758033-7e924f619b47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+      "https://scontent.fbeg1-1.fna.fbcdn.net/v/t39.30808-6/295529439_112862198167901_1509090350141059451_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeEamAub9QCjmkCfCtk30U99b6q1NUVOrppvqrU1RU6umj8KtKJivFB1XsI5HXTIpEA&_nc_ohc=s8MRAV50GJwAX_o7US-&tn=3xstOxlhC1v1kjRY&_nc_ht=scontent.fbeg1-1.fna&oh=00_AT-fidI8W3XcXCgeeNZe9W2lMb_3uQDU3RLMikahhtFvEg&oe=62F06375",
 },
 {
     image:
-      "https://www.seriouseats.com/thmb/jTyFryCxiNnlvl2v4i_5s5LyKxU=/1500x1125/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2017__04__20170423-ultra-smashed-burger-video-primary2-564cff4ad31e4d5dab5ab68e63deb601.jpg",
+      "https://scontent.fbeg1-1.fna.fbcdn.net/v/t39.30808-6/295383820_112862194834568_2621805636262881441_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeG9KYhgd-JZRjuQzUIMbWxQ2xPR7wPIqR3bE9HvA8ipHQaC1z3XITyFKIWsgXEiqdE&_nc_ohc=33q19mHdneMAX-EQQLq&_nc_ht=scontent.fbeg1-1.fna&oh=00_AT-Rs2ZihvHM2JAmy55Kbw8wyvWOxS8gonza4bAJ1mDGaA&oe=62EF640F",
 },
 {
     image:
-      "https://images.unsplash.com/photo-1524121963016-6d7476758c04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80",
-}
-  
+      "https://scontent.fbeg1-1.fna.fbcdn.net/v/t39.30808-6/295242784_112098968244224_4951203653674849151_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeGZXvJO-r7S_Zta0uGRgTlo4QPYekGJSQvhA9h6QYlJC3xZGR3kbLtLk1wKl-tk6Hg&_nc_ohc=feIPSxqzdRQAX8i42pr&_nc_ht=scontent.fbeg1-1.fna&oh=00_AT-3uQ-_IJfHDB9Vi8WoOsahZGtwQ8qUYF41tkM0BZnpqQ&oe=62EF78CC",
+},
+{
+  image:"https://scontent.fbeg1-1.fna.fbcdn.net/v/t39.30808-6/294606054_110899078364213_4616910431556444369_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeEjLKL8caxO5zQztsRG-GD02e9D_lHdJ5bZ70P-Ud0nloyuMj946xQpBzszo4E7NuY&_nc_ohc=ev5kR28lsvoAX_APzuK&_nc_ht=scontent.fbeg1-1.fna&oh=00_AT9FJj_eFA-vstEpdoNrtnTqAMGnXqiXfHkObTeF6zx5Tw&oe=62EF33CB"
+},
+  {
+    image:"https://scontent.fbeg1-1.fna.fbcdn.net/v/t39.30808-6/294432210_110484635072324_949395930204733913_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeEn1-pEptSqkeEyKPI6v72S4OEmty3VtJbg4Sa3LdW0lvpBPtbSjysjUXc9eUo7vS8&_nc_ohc=q2-KEnwpmGoAX-pJPGv&_nc_ht=scontent.fbeg1-1.fna&oh=00_AT-BqmYOp8dSivixs-dipOGj6jaAZdidqvCWZYpHUfQ2cg&oe=62EF2460"
+  },
+  {
+    image:"https://scontent.fbeg1-1.fna.fbcdn.net/v/t39.30808-6/294720275_109605791826875_4200831298183308989_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeHr9VDrwviRUIwfGWwPDfXxbuE5rN25kztu4Tms3bmTO0x6wGmItb27en9b-wOVRLo&_nc_ohc=4gXR67c57DAAX8qN9YN&_nc_ht=scontent.fbeg1-1.fna&oh=00_AT-RU42xXjua80sfKeZeBivbC--dLQAPVEi54vy7dWtPXw&oe=62F060FC"
+  },
 ];
 
 export default SliderData;
